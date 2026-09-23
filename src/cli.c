3fbdd565668e0fb9b595454057743f1f56b5913d
Amanda Ramírez <mandi.ramirez14@gmail.com>
2026-09-23 16:46:53 -0600

    cambio año

--- src/cli.c
+++ src/cli.c
@@ -38,7 +38,7 @@ int parse_arguments(int argc, char **argv, ProgramOptions *options) {
     options->show_help = 0;
     options->input_path = NULL;
     options->group_members = "Equipo Proyecto 1";
-    options->course_term = "2025-2";
+    options->course_term = "2026-2";
 
     optind = 1; /* Reset getopt */
 
